2f038269ed6036db19caec8025dcb30b858bb0c8
sjordan20 <jordanstephen949@gmail.com>
2020-02-14 16:56:11 -0700

    5pm

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Product from '../Product/Product'
 import '../Product/product.css'
+import axios from 'axios'
 
 
 
@@ -8,21 +9,21 @@ function Dashboard(props) {
 
 
 
-    // const deleteProduct = (id) => {
-    //     axios.delete(`/api/products/${id}`)
-    //         .then(res => {
-    //             ({
-    //                 inventory: res.data
-    //             })
-    //         })
-    // }
+    const deleteProduct = (id) => {
+        axios.delete(`/api/products/${id}`)
+            .then(res => {
+                ({
+                    inventory: res.data
+                })
+            })
+    }
 
     const inventoryList = props.inventory.map(element => {
         return (
             <Product
                 key={element.id}
                 inventory={element}
-            // deleteProduct={deleteProduct}
+                deleteProduct={deleteProduct}
 
 
             />
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,21 +5,26 @@ function Product(props) {
 
     return (
         <div>
-            <p className='product-box'>
+            <div className='product-box'>
                 <div className='img-container'>
                     <img className='product-img' />
                     {props.inventory.img}
                 </div>
-                <div className='name-price-container'>
-                    {props.inventory.name}
-                    {props.inventory.price}
+
+                <div className="right-side">
+
+
+                    <div className='name-price-container'>
+                        {props.inventory.name}
+                        {props.inventory.price}
+                    </div>
                     <div className="delete-container" >
 
                         <button onClick={() => props.deleteProduct(props.inventory.id)}>delete</button>
                     </div>
 
                 </div>
-            </p>
+            </div>
         </div>
     )
 
